25a36ab9b2554c1dfb315f54b78eff6caeb24e41
David Marchbanks <ptdave20@gmail.com>
2015-03-30 09:18:00 -0400

    Infinite Loop

--- src/githubtest/GithubTest.java
+++ src/githubtest/GithubTest.java
@@ -15,7 +15,8 @@ public class GithubTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        while(true)
+            System.out.println("Hello MOM!");
     }
     
 }
